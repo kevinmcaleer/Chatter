728438d0f429d468148a1c8bd532825b292f47f4
Kevin McAleer <kevinmcaleer@gmail.com>
2025-04-20 16:24:25 +0100

    create an admin page for viewing users Fixes #2

---
+++ demo03.py
@@ -0,0 +1,30 @@
+from fasthtml.common import *
+from fastlite import *
+from fasthtml import ft
+
+db = database("db.sqlite3")
+app = FastHTML(pico=True, picocss=True, hdrs=(picolink, Style(':root {--pico-font-size:90%,--pico-font-family: Pacifico, cursive;}')))
+
+class User: name:str; email:str; year_started:int
+
+users = db.create(User, pk='id')
+# users.insert(name='Kev Doe', email="kev@kevsrobots.com", year_started=2019)
+# users.insert(name='Jane Doe')
+
+# show all users
+@app.get("/")
+def show_users():
+    """ Show all users """
+    try:
+        all_users = users() # this will show all users - remember its just adding the ()
+    except NotFoundError:
+        all_users = []
+        print("No users found in the database.")
+    print(f"All Users: {all_users}")
+    return Main(
+        H1("Users"),
+        ft.Table(
+        *[ft.Tr(ft.Td(f"{user.name}"), ft.Td(f"{user.email}"), ft.Td(f"{user.year_started}")) for user in users()],
+    ))
+
+serve()
