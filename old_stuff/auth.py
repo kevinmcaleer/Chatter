09db35ceadbaed2840c3e63692bf32a4d8a43acc
Kevin McAleer <kevinmcaleer@gmail.com>
2025-05-05 14:57:37 +0100

    working authentication/

--- old_stuff/auth.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-from typing import Optional
-from fastapi import FastAPI, Depends, HTTPException, status
-from pydantic import BaseModel
-from sqlalchemy.orm import Session 
-from starlette import status
-from database import SessionLocal
