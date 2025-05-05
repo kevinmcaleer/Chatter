3448515352a66928af98bfc9c18e72f63233d8ee
Kevin McAleer <kevinmcaleer@gmail.com>
2025-05-05 20:21:36 +0100

    updated database

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Database documentation is available at:
 https://dbdocs.io/kevinmcaleer/Chatter
 
 ---
+
+create a `.env`file that contains the following variables:
+
+```env
+SECRET_KEY = "supersecretkey123"  # Replace with the real secret!
+```
--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from .database import create_db_and_tables
 from .auth import router as auth_router
 from .likes_comments import router as lc_router
+from dotenv import load_dotenv
+load_dotenv()
 
 app = FastAPI()
 app.include_router(lc_router, prefix="/interact")
